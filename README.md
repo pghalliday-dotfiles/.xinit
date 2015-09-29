0052d92aa5a75782cc117f78f16ea23bd83fe2bb
Peter Halliday <pghalliday@gmail.com>
2015-09-30 01:54:57 +0200

    initial version

---
+++ README.md
@@ -0,0 +1,13 @@
+# xinit
+
+Clone to `~/.xinit`
+
+```
+git clone git@github.com:pghalliday-dotfiles/xinit.git ~/.xinit
+```
+
+and create `~/.xinitrc`
+
+```
+echo "source ~/.xinit/config.sh" > ~/.xinitrc
+```
---
+++ config.sh
@@ -0,0 +1,14 @@
+/usr/bin/VBoxClient-all
+
+xsetroot -cursor_name left_ptr
+
+[[ -f ~/.xinit/.Xresources ]] && xrdb -merge -I$HOME/.xinit ~/.xinit/.Xresources
+
+xloadimage -onroot -fullscreen ~/background.png
+
+trayer --edge top --align right --SetDockType true --SetPartialStrut true \
+  --expand true --width 10 --transparent true --tint 0x191970 --height 12 &
+
+xscreensaver -no-splash &
+
+exec xmonad
