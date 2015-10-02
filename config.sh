e326fb73330858d46611e3c0a9f95cf1e209be45
Peter Halliday <pghalliday@gmail.com>
2015-10-02 15:27:26 +0200

    add background image

--- config.sh
+++ config.sh
@@ -4,7 +4,7 @@ xsetroot -cursor_name left_ptr
 
 [[ -f ~/.xinit/.Xresources ]] && xrdb -merge -I$HOME/.xinit ~/.xinit/.Xresources
 
-xloadimage -onroot -fullscreen ~/background.png
+xloadimage -onroot -fullscreen ~/.xinit/background.png
 
 trayer --edge top --align right --SetDockType true --SetPartialStrut true \
   --expand true --width 10 --transparent true --tint 0x191970 --height 12 &
