b0a36fd960a42aa8b8ab355b72ed3acfa67e755b
Peter Halliday <pghalliday@gmail.com>
2016-04-20 10:49:52 +0200

    set the primary display

--- detect_displays.sh
+++ detect_displays.sh
@@ -6,7 +6,7 @@ last_display=
 xrandr | grep connected | while read line; do
   display=$(echo $line | cut -f 1 -d " ")
   if [ "$last_display" == "" ]; then
-    xrandr --output $display --auto
+    xrandr --output $display --auto --primary
   else
     xrandr --output $display --auto --right-of $last_display
   fi
